9d8df0ac928854a861a7af7b18aee4c42ad9eeb2
Santosh Senapati <santosh.senapati@euromonitor.com>
2022-10-20 12:21:14 +0530

    'BankingSystem'

---
+++ C# Assignments/BankingManagementSystem/BankingManagementSystem/Current.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Principal;
+
+namespace BankManagementSystem
+{
+    public class Current : CreateAccount
+    {
+        double _balance;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine("Your current balance is {0}", _balance);
+            if (amount > 800)
+            {
+                _balance += amount;
+                Console.WriteLine("Thank you for deposite your total balance is {0}", _balance);
+            }
+            else
+            {
+                Console.WriteLine("Deposite must be greater than 800");
+            }
+        }
+        public void Withdraw(double amount)
+        {
+            if (_balance < amount)
+            {
+                Console.WriteLine("you cannot withdraw the amount due to insufficient balance");
+            }
+            else
+            {
+                _balance -= amount;
+                Console.WriteLine("your total balance is {0}", _balance);
+            }
+        }
+
+        public double Balance { get { return _balance; } }
+    }
+}
---
+++ C# Assignments/BankingManagementSystem/BankingManagementSystem/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Principal;
+
+namespace BankManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MyBank");
+            Console.WriteLine("Do you want to Create Account?? type 0");
+            int option = Convert.ToInt32(Console.ReadLine());
+            if (option == 0)
+            {
+                Console.WriteLine("Select Account type");
+                Console.WriteLine("0.Saving Account");
+                Console.WriteLine("1.Current Account");
+                int acctype = Convert.ToInt32(Console.ReadLine());
+                CreateAccount s = new CreateAccount();
+                if (acctype == 0)
+                {
+                    Saving s2 = new Saving();  // here runtitm we have to use doubt concept
+                    s.setType("saving");
+                    s.createAccount();
+                    if (s.age < 68)
+                    {
+                        Console.WriteLine("Enter amount you want to Deposite");
+                        s2.Deposit(Convert.ToInt32(Console.ReadLine()));
+
+                        Console.WriteLine("Enter amount you want to withdraw Your money");
+                        s2.Withdraw(Convert.ToInt32(Console.ReadLine()));
+
+
+                        Console.WriteLine("Enter account no to get details");
+                        s.accountDetials((Console.ReadLine()));
+                    }
+                    else
+                    {
+                        Console.WriteLine("sorry you are not eligible to create account");
+                    }
+                }
+                else if (acctype == 1)
+                {
+                    s.setType("current");
+                    s.createAccount();
+
+                    Current s2 = new Current();  
+                    if (s.age < 68)
+                    {
+                        Console.WriteLine("Enter amount you want to Deposite");
+                        s2.Deposit(Convert.ToInt32(Console.ReadLine()));
+
+                        Console.WriteLine("Enter amount you want to withdraw Your money");
+                        s2.Withdraw(Convert.ToInt32(Console.ReadLine()));
+
+                        Console.WriteLine("Enter account no to get details");
+                        s.accountDetials((Console.ReadLine()));
+                    }
+                    else
+                    {
+                        Console.WriteLine("sorry you are not eligible to create account");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You enter wrong number");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
